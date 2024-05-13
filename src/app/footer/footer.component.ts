c7958dfee0f9bc1689672708de03a961707bfef3
prosanjeet1998 <prosanjeet@innovatiqconsulting.com>
2024-05-13 23:38:41 +0530

    Initial Commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'footer-app',
+  template: '<h3>this is the footer app</h3>',
+})
+export class FooterComponent {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'headr-app',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {}
